caf84c0e211a0f10912b439683cf6985463d7696
Kyle Thomas <kthomas@hpe.com>
2024-01-22 09:17:19 +0000

    Header implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header';
+import { Grommet } from 'grommet';
+import { hpe } from 'grommet-theme-hpe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Grommet theme={hpe}>
+      <Header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +19,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </Header>
+    </Grommet>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+// HeaderNavigationExample.js
+import { AppIdentity } from '../AppIdentity';
+import React, { useContext } from 'react';
+import { Button, Header, Menu, Nav, ResponsiveContext } from 'grommet';
+
+
+
+const items = [
+  { label: 'Label 1' },
+  { label: 'Label 2' },
+  { label: 'Label 3' },
+];
+
+export const HeaderNavigationExample = () => {
+  const size = useContext(ResponsiveContext);
+  return (
+    <Header fill="horizontal">
+      <AppIdentity href="#" brand="hpe" title="App Name" />
+      {!['xsmall', 'small'].includes(size) ? (
+        <Nav direction="row" gap="small">
+          {items.map(item => (
+            <Button label={item.label} key={item.label} />
+          ))}
+        </Nav>
+      ) : (
+        <Menu label="Menu" items={items} />
+      )}
+    </Header>
+  );
+};
+
+export default HeaderNavigationExample
