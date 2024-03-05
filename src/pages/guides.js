8b066cf38541112fced12e7df6ef57f289de9c19
Kyle Thomas <kthomas@hpe.com>
2024-03-05 21:44:04 +0000

    Update guides.js

--- src/pages/guides.js
+++ src/pages/guides.js
@@ -1,33 +1,6 @@
 import React from 'react';
 import { Accordion, AccordionPanel, Box, Text, Heading, List } from 'grommet';
 
-// const Guides = () => {
-//     return <Box direction="column" gap="medium">
-//                 <div>
-//                     <Text size="3xl">Welcome to our Sales Incentives Leaderboard platform!</Text>
-//                     <Paragraph> Where excellence in sales is not only recognized but also rewarded! This user guide will walk you through the exciting features and easy-to-use functionalities of our system, designed to motivate and celebrate your outstanding sales achievements.</Paragraph>
-
-//                     <h2>Getting Started: </h2>
-//                     <p>To begin your journey on the Sales Incentives Leaderboard, simply log in to your account. If you're a new user, follow the straightforward registration process to create your profile. Once logged in, you'll find a user-friendly dashboard where you can easily navigate through the various sections.</p>
-
-//                     <h2>Adding Your Sales:</h2>
-//                     <p>Navigate to the "Submit Sales" section to input your sales data. Here, you can enter relevant details such as the OPE, the account name, and the business unit. As you submit each sale, our system will automatically convert the sales data into points, reflecting your contribution to the overall success of the team.</p>
-
-//                     <h2>Leaderboard and Points Accumulation:</h2>
-//                     <p>The heart of our platform lies in the dynamic leaderboard, showcasing the top performers based on accumulated points. Points are assigned based on your sales, giving you a fair and transparent measure of your success. Keep an eye on the leaderboard to track your progress and see how you compare to your colleagues.</p>
-
-//                     <h2>Competition and Rewards:</h2>
-//                     <p>As the competition heats up, so do the rewards! The individual with the highest points at the end of the designated period will be crowned the Sales Champion and will receive an exciting prize as a token of appreciation for their hard work and dedication.</p>
-                    
-//                     <h2>Conclusion:</h2>
-//                     <p>We are thrilled to have you on board, and we can't wait to witness your success on our Incentives Leaderboard. Let the friendly competition and rewards drive you to new heights in your sales journey. If you have any questions or need assistance, reach out to your HPE Partner. Good luck, and may the highest sales and points lead you to victory! </p>
-//                 </div>;
-//             </Box>
-//   };
-
-// import React from 'react';
-// import { Accordion, AccordionPanel, Box } from 'grommet';
-
 export const Guides = ({ ...rest }) => {
   const pad = 'small';
 
